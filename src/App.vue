0b1c36f333e726b71845f73ef9e1b249b0c337c9
Kalina12345 <167115502+Kalina12345@users.noreply.github.com>
2024-04-15 19:46:58 +0300

    routes coment to

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Главная</router-link> |
+      <router-link to="/catalog">Каталог продукции</router-link>
+      <router-link to="/program">подбор программы</router-link>
     </nav>
     <router-view/>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,21 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/catalog',
+    name: 'catalog',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    component: () => import(/* webpackChunkName: "about" */ '../views/CatalogView.vue')
+  },
+  {
+    path: '/program',
+    name: 'program',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/ProgramView.vue')
+  },
 ]
 
 const router = new VueRouter({
